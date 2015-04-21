2b3824629ba3821a7f811c00bbdd6430749a1485
Sebastiaan Dirks <513811@lln.corlaercollege.nl>
2015-04-21 07:57:05 +0000

    New things

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <?php include "php/base.php"; ?>
+    </head>
     
+    <body>
+        <?php include "php/navbar.php"; ?>
+    </body>
 </html>
---
+++ php/base.php
@@ -0,0 +1,5 @@
+<!-- Stylesheet -->
+<link rel="stylesheet" href="css/materialize.min.css" type="text/css" />
+
+<!-- Script -->
+<script type="text/javascript" src="js/materialize.min.js"></script>
